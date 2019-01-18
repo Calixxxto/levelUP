db199cc659ce092b4f4cc0342a709456fe7487c8
KseniaBolshakova <ksenbols@gmail.com>
2019-01-18 20:21:29 +0400

    first

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+    private Example example = new Example();
+    private File temp;
+
+    @Before
+    public void prepare() throws IOException {
+        example.setValue(777);
+        temp = File.createTempFile("test", "test");
+    }
+
+    @After
+    public void cleanup(){
+        if (temp != null) {
+            temp.delete();
+        }
+    }
+
+    @Test
+    public void testSum(){
+        assertEquals(3, new Example().sum(1, 2));
+    }
+
+    @Test
+    public void testSum2(){
+        assertEquals(3, new Example().sum(1, 2));
+    }
+    @Test
+    public void testSum3(){
+        assertEquals(3, new Example().sum(1, 2));
+    }
+}
